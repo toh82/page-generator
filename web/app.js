8b938d7da4929639e50a30a201b617398bbfe47a
Tobias <tobias.hartmann@sitewards.com>
2016-06-04 10:45:25 +0200

    feat: add methods to get template configs from their folders

--- web/app.js
+++ web/app.js
@@ -1,40 +1,101 @@
-var express    = require('express');
-var app        = express();
-var Handlebars = require('handlebars');
-var fs         = require('fs');
+var express = require('express')
+var app = express()
+var Handlebars = require('handlebars')
+var fs = require('fs')
+var glob = require('glob')
 
-const fceStorePath = './content-elements';
+/** @type {string} */
+const templateStorePath = './resources/templates'
 
-function loadFceTemplate (templateName) {
-  var fceTemplatePath = fceStorePath + '/' + templateName + '/' + templateName + '.html';
-  return fs.readFileSync(fceTemplatePath).toString();
+/** @type {string} */
+const templateConfigFileName = 'config.json'
+
+/**
+ * @param {string} dir
+ * @returns {Array}
+ */
+function getTemplateConfigs (dir) {
+  var files = glob.sync(dir + '/**/' + templateConfigFileName)
+
+  var templateConfigs = []
+  files.forEach(function (file) {
+    var json = JSON.parse(fs.readFileSync(file, 'utf8'))
+    templateConfigs.push(json)
+  })
+
+  return templateConfigs
 }
 
-function renderFceHtml (fceData, rawFceTemplateString) {
-  var template = Handlebars.compile(rawFceTemplateString);
-  return template(fceData);
+/**
+ * @returns {Array}
+ */
+function getTemplatesContainerConfigs () {
+  var templateContainerDir = templateStorePath + '/container'
+  return getTemplateConfigs(templateContainerDir)
 }
 
-function renderPageFce (res, pageFce) {
-  res.set('Content-Type', 'text/html');
+/**
+ * @returns {Array}
+ */
+function getTemplatesContentConfigs () {
+  var templateContentDir = templateStorePath + '/content'
+  return getTemplateConfigs(templateContentDir)
+}
 
-  pageFce.forEach(function (fceConfig) {
-    var fceTemplate = loadFceTemplate(fceConfig.template);
-    var fceHtml     = renderFceHtml(fceConfig, fceTemplate);
+/**
+ * @returns {Array}
+ */
+function getTemplatesPageConfigs () {
+  var templatePageDir = templateStorePath + '/page'
+  return getTemplateConfigs(templatePageDir)
+}
 
-    res.write(fceHtml);
-  });
+/**
+ * @param {object} template
+ * @returns {string}
+ */
+function loadHtmlTemplateFile (template) {
+  var templatePath = templateStorePath + '/' + template.type + '/' + template.name + '/' + template.name + '.html'
+  return fs.readFileSync(templatePath).toString()
+}
 
-  res.end();
+/**
+ * @param {object} fceData
+ * @param {string} rawTemplateString
+ * @returns {string}
+ */
+function renderHtmlTemplate (fceData, rawTemplateString) {
+  var template = Handlebars.compile(rawTemplateString)
+  return template(fceData)
+}
+
+function renderPage (res, pageConfig) {
+  var pageFceConfig = pageConfig.fce
+  var fceHtmlString = ''
+
+  pageFceConfig.forEach(function (fceConfig) {
+    var fceTemplate = loadHtmlTemplateFile(fceConfig.template)
+    var fceHtml = renderHtmlTemplate(fceConfig, fceTemplate)
+
+    fceHtmlString = fceHtmlString + fceHtml
+  })
+
+  pageConfig.content = fceHtmlString
+
+  var pageTemplate = loadHtmlTemplateFile(pageConfig.template)
+  var renderedPageHtml = renderHtmlTemplate(pageConfig, pageTemplate)
+
+  res.set('Content-Type', 'text/html')
+  res.write(renderedPageHtml)
+  res.end()
 }
 
 app.get('/', function (req, res) {
-  var pageConfig = require('../example-page/config');
-  var pageFce    = pageConfig.fce;
-
-  renderPageFce(res, pageFce);
-});
+  var pageConfig = require('../example-page/config')
+  renderPage(res, pageConfig)
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 3000!')
+  console.log(getTemplatesContainerConfigs())
+})
