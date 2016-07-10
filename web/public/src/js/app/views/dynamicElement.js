c9454b0b06469d024e36537f03726df1f9d2b6e4
Tobias <tobias.hartmann@sitewards.com>
2016-07-10 14:09:35 +0200

    feat(dynamicView): add toolbar html and add class to the outer wrapping div

--- web/public/src/js/app/views/dynamicElement.js
+++ web/public/src/js/app/views/dynamicElement.js
@@ -1,9 +1,15 @@
 import Handlebars from 'handlebars'
 import Marionette from 'marionette'
+import toolbarTemplate from 'text!templates/toolbar.html'
 
 export default Marionette.ItemView.extend({
   initialize: function () {
+    this.$el.addClass('pg-elementId[' + this.model.get('elementId') + ']')
+
     var templateData = this.model.get('template')
     this.template = Handlebars.compile(templateData.html)
+  },
+  onRender: function () {
+    this.$el.prepend(toolbarTemplate)
   }
 })
